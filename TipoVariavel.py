eb5d3a25beada6fb29bb30be7714436f0f38e5b6
Felype <132705654+FelypeDantas@users.noreply.github.com>
2023-10-27 09:53:24 -0300

    Add files via upload

---
+++ TipoVariavel.py
@@ -0,0 +1,12 @@
+#em Python
+codigo = 10
+salario = 1500.00
+nome = "José"
+situacao = True
+
+tipo = type (salario)
+
+print(salario)
+print(tipo)
+
+print("Código: ", codigo, "Nome: ", nome, "O salário atual é de R$", salario)
